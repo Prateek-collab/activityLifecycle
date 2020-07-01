2747d6e2f700323f13594bc93672b20b15c1e9d6
Prateek Ranjan <prateekranjan14059827@gmail.com>
2020-07-01 11:39:15 +0530

    Initial commit

---
+++ app/src/main/java/com/prateek/activitylifecycle/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.prateek.activitylifecycle
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_relative_layout)
+    }
+
+    override fun onStart() {
+        super.onStart()
+    }
+
+    override fun onResume() {
+        super.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+    }
+
+    override fun onStop() {
+        super.onStop()
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+    }
+}
